2acc6a5854badf6a38305031715a7b40c863a6b3
Edgar Gonzalez <edgard.gonzalez@aurea.com>
2018-08-18 22:35:30 -0300

    feat: create calendar module

--- src/app/modules/calendar/calendar.module.ts
+++ src/app/modules/calendar/calendar.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CalendarGeneratorComponent } from './pages/calendar-generator/calendar-generator.component';
 import { SharedModule } from '../../shared/shared.module';
 import { CalendarRoutingModule } from './calendar.routing.module';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   imports: [
     SharedModule,
     CalendarRoutingModule,
   ],
-  declarations: [CalendarGeneratorComponent]
+  declarations: [
+    CalendarGeneratorComponent,
+    CalendarComponent,
+  ],
+  entryComponents: [
+    CalendarComponent,
+  ]
 })
 export class CalendarModule {}
